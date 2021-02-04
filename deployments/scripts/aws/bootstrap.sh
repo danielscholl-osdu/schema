91b2635c9a420f1db86e1b998e1decc962dd1dfb
Matt Wise <wsmatth@amazon.com>
2021-02-04 11:50:43 -0600

    add deployments_base_dir env var to support running from different directories

--- deployments/scripts/aws/bootstrap.sh
+++ deployments/scripts/aws/bootstrap.sh
@@ -1,3 +1,17 @@
+#Required Env Variables
+
+#AWS_BASE_URL
+#AWS_DEPLOYMENTS_SUBDIR
+#AWS_REGION
+#AWS_COGNITO_CLIENT_ID
+#AWS_COGNITO_AUTH_FLOW
+#AWS_COGNITO_AUTH_PARAMS_USER
+#AWS_COGNITO_AUTH_PARAMS_PASSWORD
+
+if [ -z "$DEPLOYMENTS_BASE_DIR" ];
+then export DEPLOYMENTS_BASE_DIR=deployments;
+fi
+
 echo $AWS_BASE_URL
 export AWS_SCHEMA_SERVICE_URL=$AWS_BASE_URL/api/schema-service/v1/schema
 BEARER_TOKEN=`python $AWS_DEPLOYMENTS_SUBDIR/Token.py`
@@ -5,4 +19,4 @@ echo $BEARER_TOKEN
 export BEARER_TOKEN=$BEARER_TOKEN
 export APP_KEY=""
 export DATA_PARTITION=common
-python deployments/scripts/DeploySharedSchemas.py -l load_sequence.1.0.0.json -u $AWS_SCHEMA_SERVICE_URL
+python3 $DEPLOYMENTS_BASE_DIR/scripts/DeploySharedSchemas.py -l load_sequence.1.0.0.json -u $AWS_SCHEMA_SERVICE_URL
