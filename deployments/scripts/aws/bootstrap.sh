b6885b63f22c43373a740c5e72e3f51f881718a1
Matt Wise <wsmatth@amazon.com>
2021-02-04 09:53:40 -0600

    add schema deployment scripts to artifacts

--- deployments/scripts/aws/bootstrap.sh
+++ deployments/scripts/aws/bootstrap.sh
@@ -1,8 +1,18 @@
+#Required Env Variables
+
+#AWS_BASE_URL
+#AWS_DEPLOYMENTS_SUBDIR
+#AWS_REGION
+#AWS_COGNITO_CLIENT_ID
+#AWS_COGNITO_AUTH_FLOW
+#AWS_COGNITO_AUTH_PARAMS_USER
+#AWS_COGNITO_AUTH_PARAMS_PASSWORD
+
 echo $AWS_BASE_URL
 export AWS_SCHEMA_SERVICE_URL=$AWS_BASE_URL/api/schema-service/v1/schema
-BEARER_TOKEN=`python $AWS_DEPLOYMENTS_SUBDIR/Token.py`
+BEARER_TOKEN=`python3 $AWS_DEPLOYMENTS_SUBDIR/Token.py`
 echo $BEARER_TOKEN
 export BEARER_TOKEN=$BEARER_TOKEN
 export APP_KEY=""
 export DATA_PARTITION=common
-python deployments/scripts/DeploySharedSchemas.py -l load_sequence.1.0.0.json -u $AWS_SCHEMA_SERVICE_URL
+python3 deployments/scripts/DeploySharedSchemas.py -l load_sequence.1.0.0.json -u $AWS_SCHEMA_SERVICE_URL
