81cec12758dad2ecc718bdd97ad6f19ac21fdb21
Aman Verma <amaverma@microsoft.com>
2021-06-30 19:26:39 +0000

    Adding integration tests schemas/system endpoint

--- provider/schema-azure/src/main/java/org/opengroup/osdu/schema/azure/auth/AuthorizationFilterSP.java
+++ provider/schema-azure/src/main/java/org/opengroup/osdu/schema/azure/auth/AuthorizationFilterSP.java
@@ -1,5 +1,6 @@
 package org.opengroup.osdu.schema.azure.auth;
 
+import org.opengroup.osdu.core.common.model.http.DpsHeaders;
 import org.opengroup.osdu.schema.azure.interfaces.IAuthorizationServiceForServicePrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,9 +11,13 @@ import org.springframework.web.context.annotation.RequestScope;
 public class AuthorizationFilterSP {
 
     @Autowired
+    private DpsHeaders headers;
+
+    @Autowired
     private IAuthorizationServiceForServicePrincipal authorizationService;
 
     public boolean hasPermissions() {
+        headers.put(DpsHeaders.USER_EMAIL, "ServicePrincipalUser");
         return authorizationService.isDomainAdminServiceAccount();
     }
 }
---
+++ testing/schema-test-azure/src/test/java/org/opengroup/osdu/schema/stepdefs/LocalSetupStepDef.java
@@ -0,0 +1,12 @@
+package org.opengroup.osdu.schema.stepdefs;
+
+import io.cucumber.java8.En;
+import lombok.extern.java.Log;
+
+@Log
+public class LocalSetupStepDef implements En {
+
+    public LocalSetupStepDef() {
+
+    }
+}
