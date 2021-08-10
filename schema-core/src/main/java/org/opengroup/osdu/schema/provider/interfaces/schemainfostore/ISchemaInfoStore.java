12bae0b8451c7b866cdb254e5935874e9243faac
Aman Verma <amaverma@microsoft.com>
2021-08-10 12:33:46 +0530

    updating the interface

--- schema-core/src/main/java/org/opengroup/osdu/schema/provider/interfaces/schemainfostore/ISchemaInfoStore.java
+++ schema-core/src/main/java/org/opengroup/osdu/schema/provider/interfaces/schemainfostore/ISchemaInfoStore.java
@@ -9,20 +9,45 @@ import org.opengroup.osdu.schema.exceptions.NotFoundException;
 import org.opengroup.osdu.schema.model.QueryParams;
 import org.opengroup.osdu.schema.model.SchemaInfo;
 import org.opengroup.osdu.schema.model.SchemaRequest;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public interface ISchemaInfoStore {
 
     SchemaInfo updateSchemaInfo(SchemaRequest schema) throws ApplicationException, BadRequestException;
 
+    default SchemaInfo updatePublicSchemaInfo(SchemaRequest schema) throws ApplicationException, BadRequestException {
+        throw new NotImplementedException();
+    }
+
     SchemaInfo createSchemaInfo(SchemaRequest schema) throws ApplicationException, BadRequestException;
 
+    default SchemaInfo createPublicSchemaInfo(SchemaRequest schema) throws ApplicationException, BadRequestException {
+        throw new NotImplementedException();
+    }
+
     SchemaInfo getSchemaInfo(String schemaId) throws ApplicationException, NotFoundException;
+
+    default SchemaInfo getPublicSchemaInfo(String schemaId) throws ApplicationException, NotFoundException {
+        throw new NotImplementedException();
+    }
 
     String getLatestMinorVerSchema(SchemaInfo schemaInfo) throws ApplicationException;
 
     List<SchemaInfo> getSchemaInfoList(QueryParams queryParams, String tenantId) throws ApplicationException;
 
+    default List<SchemaInfo> getPublicSchemaInfoList(QueryParams queryParams) throws ApplicationException {
+        throw new NotImplementedException();
+    }
+
     boolean isUnique(String schemaId, String tenantId) throws ApplicationException;
 
+    default boolean isUnique(String schemaId) throws ApplicationException {
+        throw new NotImplementedException();
+    }
+
     boolean cleanSchema(String schemaId) throws ApplicationException;
+
+    default boolean cleanPublicSchema(String schemaId) throws ApplicationException {
+        throw new NotImplementedException();
+    }
 }
