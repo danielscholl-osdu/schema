7d75421d94c946d84d42bd5a239099da107c576e
Abhishek Patil <abpatil@microsoft.com>
2022-02-12 11:26:58 +0530

    Add logic to terminate istio sidecar

--- deployments/scripts/azure/bootstrap.sh
+++ deployments/scripts/azure/bootstrap.sh
@@ -1,4 +1,48 @@
-export AZURE_SCHEMA_URL=https://${AZURE_DNS_NAME}/api/schema-service/v1/schema
+# Cleanup function
+cleanup() {
+  echo "Terminating istio sidecar"
+  curl -X POST "http://localhost:15020/quitquitquit"
+}
+
+# Function to check istio sidecar readiness
+checkIstioSidecarReadiness() {
+	echo "Wait for istio sidecar to be ready..."
+	
+	max_retry_count=18
+	current_retry_count=0
+	sidecar_ready_status=0
+	
+	while [ ${current_retry_count} -lt ${max_retry_count} ];
+	do
+		status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:15021/healthz/ready)
+		if [[ "$status_code" -ne 200 ]] ; then
+		sleep 10s
+		echo "Istio sidecar not ready yet. Sleeping for 10s..."
+		else
+		sidecar_ready_status=1
+		break
+		fi
+		current_retry_count=$(expr $current_retry_count + 1)
+	done
+	
+	if [[ ${sidecar_ready_status} -ne 1 ]]; then
+		echo "Timed out waiting for istio sidecar to be ready. Exiting..."
+		exit 1
+	fi
+	
+	echo "Istio sidecar is ready..."
+}
+
+trap cleanup 0 1 2 3 6
+
+checkIstioSidecarReadiness
+
+if [[ -z "${NAMESPACE}" ]]; then
+  NAMESPACE="osdu-azure"
+fi
+
+export AZURE_SCHEMA_URL="http://schema.${NAMESPACE}.svc.cluster.local/api/schema-service/v1/schema/"
+
 currentStatus="success"
 currentMessage="All schemas uploaded successfully"
 BEARER_TOKEN=`python $AZURE_DEPLOYMENTS_SUBDIR/Token.py`
