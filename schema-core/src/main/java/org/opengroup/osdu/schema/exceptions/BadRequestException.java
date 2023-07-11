58f3a68ed68be73df1d96c6fef813bcd1af2bf18
Shreya Shah <shreya_shah@epam.com>
2023-07-11 09:19:05 +0000

    Fixed Preship Issue #521 - Azure PUT /api/schema-service/v1/schemas/system

--- schema-core/src/main/java/org/opengroup/osdu/schema/exceptions/BadRequestException.java
+++ schema-core/src/main/java/org/opengroup/osdu/schema/exceptions/BadRequestException.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class BadRequestException extends Exception {
+public class BadRequestException extends RuntimeException {
 
 	private final HttpStatus status;
 	private final String errorMsg;
