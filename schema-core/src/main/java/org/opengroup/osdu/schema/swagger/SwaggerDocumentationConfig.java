01b3bf259fef7d52a3b6297001156f6f9532285b
Lawrence Chan <lach@microsoft.com>
2024-02-14 14:58:14 +0000

    Use full URL instead of relative path

--- schema-core/src/main/java/org/opengroup/osdu/schema/swagger/SwaggerDocumentationConfig.java
+++ schema-core/src/main/java/org/opengroup/osdu/schema/swagger/SwaggerDocumentationConfig.java
@@ -14,34 +14,69 @@ limitations under the License.*/
 
 package org.opengroup.osdu.schema.swagger;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(
-                title = "${api.title}",
-                description = "${api.description}",
-                version = "${api.version}",
-                contact = @Contact(name = "${api.contact.name}", email = "${api.contact.email}"),
-                license = @License(name = "${api.license.name}", url = "${api.license.url}")),
-        servers = @Server(url = "${api.server.url}"),
-        security = @SecurityRequirement(name = "Authorization"),
-        tags = {
-                @Tag(name = "schema-api", description = "Schema API - Core Schema related endpoints"),
-                @Tag(name = "system-schema-api", description = "System Schema API - System Schema related endpoints"),
-                @Tag(name = "info", description = "Version info endpoint")
-        }
-)
-@SecurityScheme(name = "Authorization", scheme = "bearer", bearerFormat = "Authorization", type = SecuritySchemeType.HTTP)
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class SwaggerDocumentationConfig {
 
+        @Autowired
+        private SwaggerConfigurationProperties configurationProperties;
+
+        @Bean
+        public OpenAPI customOpenAPI() {
+
+                SecurityScheme securityScheme = new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("Authorization")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization");
+                final String securitySchemeName = "Authorization";
+                SecurityRequirement securityRequirement = new SecurityRequirement().addList(securitySchemeName);
+                Components components = new Components().addSecuritySchemes(securitySchemeName, securityScheme);
+
+                OpenAPI openAPI = new OpenAPI()
+                                .addSecurityItem(securityRequirement)
+                                .components(components)
+                                .info(apiInfo())
+                                .tags(tags());
+
+                if (configurationProperties.isApiServerFullUrlEnabled())
+                        return openAPI;
+                return openAPI
+                        .servers(Arrays.asList(new Server().url(configurationProperties.getApiServerUrl())));
+        }
+
+        private List<Tag> tags() {
+                List<Tag> tags = new ArrayList<>();
+                tags.add(new Tag().name("schema-api").description("Schema API - Core Schema related endpoints"));
+                tags.add(new Tag().name("system-schema-api").description("System Schema API - System Schema related endpoints"));
+                tags.add(new Tag().name("info").description("Version info endpoint"));
+                return tags;
+        }
+
+        private Info apiInfo() {
+                return new Info()
+                        .title(configurationProperties.getApiTitle())
+                        .description(configurationProperties.getApiDescription())
+                        .version(configurationProperties.getApiVersion())
+                        .license(new License().name(configurationProperties.getApiLicenseName()).url(configurationProperties.getApiLicenseUrl()))
+                        .contact(new Contact().name(configurationProperties.getApiContactName()).email(configurationProperties.getApiContactEmail()));
+        }
 }
