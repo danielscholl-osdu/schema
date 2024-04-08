18d7b74c3a576e4539d4c3c741d900eeb67c34ea
Ashwani Pandey <ashwani.pandey3@ibm.com>
2024-04-08 15:37:01 +0530

    Gc changes updated

--- provider/schema-gc/src/main/java/org/opengroup/osdu/schema/security/WebSecurity.java
+++ provider/schema-gc/src/main/java/org/opengroup/osdu/schema/security/WebSecurity.java
@@ -17,18 +17,27 @@
 
 package org.opengroup.osdu.schema.security;
 
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.httpBasic().disable().csrf()
-        .disable(); // disable default authN. AuthN handled by endpoints proxy
+@Configuration
+public class WebSecurity {
+  @Bean
+  public SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
+    http
+            .cors(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+            .httpBasic(withDefaults());
+    return http.build();
   }
 }
